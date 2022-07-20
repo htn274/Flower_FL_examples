2447dcc8858f87a42d65dd28782ef02c199d0dd9
Thien Nu Hoang <htnu@Nus-MacBook-Pro.local>
2022-07-20 15:15:52 +0900

    update simulation

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
-from server import parse_args, get_eval_fn
+from custom_strategy import SaveModelStrategy
+from server import parse_args, get_eval_fn, save_hist
 from client import FlowerClient
 import flwr as fl
 
@@ -16,21 +17,23 @@ if __name__ == '__main__':
         }
         return config
 
-    strategy = fl.server.strategy.FedAvg(
+    strategy = SaveModelStrategy(
         fraction_fit=args.sample_fraction,
         fraction_eval=args.sample_fraction,
         min_fit_clients=args.num_clients*args.sample_fraction,
         min_eval_clients=args.num_clients*args.sample_fraction,
         min_available_clients=args.num_clients,  # All clients should be available
         on_fit_config_fn=fit_config,
+        accept_failures=False,
         eval_fn=get_eval_fn(), # centralised testset evaluation of global model
+        save_dir=args.save_dir,
     )
     def client_fn(cid: str):
         # create a single client instance
         return FlowerClient(cid, args.fed_dir, verbose=False)
 
     ray_init_args = {"include_dashboard": False}   
-    fl.simulation.start_simulation(
+    hist = fl.simulation.start_simulation(
         client_fn=client_fn,
         num_clients=args.num_clients,
         client_resources=client_resources,
@@ -38,3 +41,5 @@ if __name__ == '__main__':
         strategy=strategy,
         ray_init_args=ray_init_args,
     )
+    print("Saving training history")
+    save_hist(hist, args)
